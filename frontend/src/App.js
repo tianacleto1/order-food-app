6bb6240e48cee0dd966dffd2894d54bd07c785ff
Tiago Silva <tianacleto@hotmail.com>
2022-02-12 21:51:00 -0300

    implemented navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>AWESOME PIZZA</h1>
+      <NavBar />
     </div>
   );
 }
