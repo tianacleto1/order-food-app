993b81ba63a2bc1fb5bccbb8715c8141425318b2
Tiago Silva <tianacleto@hotmail.com>
2022-02-15 18:09:15 -0300

    implemented modal for when the pizza card is clicked

--- frontend/src/components/Pizza.js
+++ frontend/src/components/Pizza.js
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
 
+export default function Pizza({ pizza }) {
 
-export default function Pizza({pizza}) {
-    
     const [quantity, setQuantity] = useState(1);
     const [variant, setVariant] = useState('small');
 
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
-        <div style={{margin: '70px'}} className="shadow-lg p-3 mb-5 bg-white rounded">
-            <h1>{pizza.name}</h1>
-            <img src={pizza.image} className="img-fluid" style={{height: '200px', width: '200px'}}/>
-            
+        <div style={{ margin: '70px' }} className="shadow-lg p-3 mb-5 bg-white rounded">
+            <div onClick={handleShow}>
+                <h1>{pizza.name}</h1>
+                <img src={pizza.image} className="img-fluid" style={{ height: '200px', width: '200px' }} />
+            </div>
+
             <div className="flex-container">
                 <div className="w-100 m-1">
                     <p>Variants</p>
-                    <select className="form-control" value={variant} onChange={(e)=>{setVariant(e.target.value)}}>
+                    <select className="form-control" value={variant} onChange={(e) => { setVariant(e.target.value) }}>
                         {pizza.variants.map(variant => {
                             return <option value={variant}>{variant}</option>
                         })}
@@ -22,9 +29,9 @@ export default function Pizza({pizza}) {
                 </div>
                 <div className="w-100 m-1">
                     <p>Quantity</p>
-                    <select className="form-control" value={quantity} onChange={(e)=>{setQuantity(e.target.value)}}>
-                        {[...Array(10).keys()].map((x, i)=>{
-                            return <option value={i+1}>{i+1}</option>
+                    <select className="form-control" value={quantity} onChange={(e) => { setQuantity(e.target.value) }}>
+                        {[...Array(10).keys()].map((x, i) => {
+                            return <option value={i + 1}>{i + 1}</option>
                         })}
                     </select>
                 </div>
@@ -38,6 +45,21 @@ export default function Pizza({pizza}) {
                     <button className="btn">ADD TO CART</button>
                 </div>
             </div>
+
+            <Modal show={show}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{pizza.name}</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    <img src={pizza.image} alt="image not found" className="img-fluid" style={{height: '400px'}}/>
+                    <p className="mt-2">{pizza.description}</p>
+                </Modal.Body>
+
+                <Modal.Footer>
+                    <button className="btn" onClick={handleClose}>CLOSE</button>
+                </Modal.Footer>
+            </Modal>
         </div>
     )
 }
